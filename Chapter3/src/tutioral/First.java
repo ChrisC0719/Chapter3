4ca2951edaf229a3919559292139a7262e8abe3b
ChrisC0719 <31348437+ChrisC0719@users.noreply.github.com>
2017-09-11 09:56:25 -0500

    update

---
+++ Chapter3/src/tutioral/First.java
@@ -0,0 +1,11 @@
+package tutioral;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Second.displayAdress();
+	}
+	 
+}
---
+++ Chapter3/src/tutioral/Second.java
@@ -0,0 +1,13 @@
+package tutioral;
+
+public class Second {
+
+	public static void displayAdress()
+	{
+		System.out.println("XYZ COompany");
+		System.out.println("8900 U.S. Hwy 14");
+		System.out.println("Crystal Lake, IL 60014");
+		System.out.println("First Java application");
+		
+	}
+}
