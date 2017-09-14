219a682692441db4934642eadf071d23f35de221
ChrisC0719 <31348437+ChrisC0719@users.noreply.github.com>
2017-09-14 09:29:24 -0500

    update

---
+++ Chapter3/src/exercises/Percentages.java
@@ -0,0 +1,24 @@
+package exercises;
+
+public class Percentages {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		int num1 = (int) 2.0;
+		int num2 = (int) 5.0;
+	
+			computePercent(num1, num2);
+	}
+	public static void computePercent(int x, int y)
+	{
+		int firstNumber;
+		int secondNumber;
+		
+		
+
+	}
+
+	
+}
