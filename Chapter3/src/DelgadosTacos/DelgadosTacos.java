7eb86b79632c7be2612607e50c01ede9ea614591
ChrisC0719 <31348437+ChrisC0719@users.noreply.github.com>
2017-09-18 08:23:35 -0500

    update

--- Chapter3/src/DelgadosTacos/DelgadosTacos.java
+++ Chapter3/src/DelgadosTacos/DelgadosTacos.java
@@ -1,15 +1,44 @@
 package DelgadosTacos;
 
+import java.util.Scanner;
+
 public class DelgadosTacos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
 		double tacoPrice = 2.55;
 		double softDrinkPrice = 1.50;
 		double nachoPrice = 3.00;
 		double enchiladasPrice = 4.50;
+		double frenchFriesPrice = 1.99;
+		double walkingTacoPrice = 3.99;
+		double saladPrice = 1.50;
+		double pretezelPrice = 3.05;
+		double fruitSnacksPrice = 1.00;
+		double chipsPrice = 1.59;
+		
+		double tacoAmountOrder = 10;
+		double chipAmountOrder = 11;
+		double saladAmountOrder = 3;
+		double fruitSnacksAmountOrder = 6;
+		double softDrinkAmountOrder = 15;
+		double nachoAmountOrder = 4;
+		double pretezelAmountOrder = 3;
+		double walkingTacoAmountOrder = 11;
+		double enchiladasAmountOrder = 22;
+		double frenchFriesAmountOrder = 42;
+		
+		
+		 
+		menu();
+	}
+	public static void menu()
+	{
+		System.out.print("How many tacos do you want? >>>> ");
+		tacoAmountOrder = input.nextDouble();
+		tacoPriceTotal = tacoAmountOrder * tacoPrice;
 		
 	}
+		
 
 }
